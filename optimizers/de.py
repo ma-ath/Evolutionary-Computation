558c57fc39cf4821aa07f68c2153016282f8feb4
Rafael DAvila <rdgama7@gmail.com>
2026-05-18 18:03:44 +0900

    Started implementing Differential Evolution

--- optimizers/de.py
+++ optimizers/de.py
@@ -9,6 +9,10 @@ class DifferentialEvolution(PopulationOptimizer):
                  population_size,
                  dimensions,
                  bounds,
+                 F = 0.5,
+                 mutation,
+                 differences = 1,
+                 crossover,
                  **kwargs):
         super().__init__(problem,
                          direction,
@@ -16,6 +20,23 @@ class DifferentialEvolution(PopulationOptimizer):
                          dimensions=dimensions,
                          bounds=bounds,
                          **kwargs)
+        
+        self.F = F
+        self.mutation = mutation
+        self.differences = differences
+        self.crossover = crossover
     
     def evolve(self):
-        pass
+        
+        r1_idx = np.random.randint(low=0, high=self.population_size, size=self.population_size)
+        r2_idx = np.random.randint(low=0, high=self.population_size, size=self.population_size)
+        r3_idx = np.random.randint(low=0, high=self.population_size, size=self.population_size)
+        i_idx = np.arange(self.population_size)
+
+        while np.any(r1_idx == i_idx):
+            mask = r1_idx == i_idx
+            r1_idx[mask] = np.random.randint(low=0, high=self.population_size, size=np.sum(mask))
+
+        while np.any(r2_idx == i_idx | r2_idx == r1_idx):
+            mask = (r2_idx == i_idx | r2_idx == r1_idx):
+            r2_idx[mask] = np.random.randint(low=0, high=self.population_size, size=np.sum(mask))  
